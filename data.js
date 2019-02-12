32f89b13d79261096f9cef0a286887c00be4cb6b
liuzhuoling <liuzhuoling2011@hotmail.com>
2019-02-12 10:07:49 +0800

    init

---
+++ data.js
@@ -0,0 +1,192 @@
+var map_data = [
+    {
+        "marker_img": "705574348478825170.jpg",
+        "position": [
+            139.787379,
+            35.6792
+        ],
+        "content": "1234567",
+        "imgs": [
+            {
+                "src": "705574348478825170.jpg",
+                "caption": ""
+            },
+            {
+                "src": "desktop.png",
+                "caption": ""
+            },
+            {
+                "src": "picture1.jpg",
+                "caption": ""
+            },
+            {
+                "src": "picture11.jpg",
+                "caption": ""
+            },
+            {
+                "src": "picture2.jpg",
+                "caption": ""
+            }
+        ],
+        "city": "日本",
+        "title": "日本",
+        "order": 0
+    },
+    {
+        "marker_img": "123.jpg",
+        "position": [
+            101.71288,
+            3.15618
+        ],
+        "content": "2345678",
+        "imgs": [
+            {
+                "src": "123.jpg",
+                "caption": ""
+            },
+            {
+                "src": "258403945913290392.jpg",
+                "caption": ""
+            },
+            {
+                "src": "30.jpg",
+                "caption": ""
+            },
+            {
+                "src": "31.jpg",
+                "caption": ""
+            },
+            {
+                "src": "32.jpg",
+                "caption": ""
+            },
+            {
+                "src": "321.jpg",
+                "caption": ""
+            }
+        ],
+        "city": "马来西亚",
+        "title": "马来西亚",
+        "order": 1
+    },
+    {
+        "marker_img": "picture38.jpg",
+        "position": [
+            103.66769,
+            1.319586
+        ],
+        "content": "3456789",
+        "imgs": [
+            {
+                "src": "picture38.jpg",
+                "caption": ""
+            },
+            {
+                "src": "picture39.jpg",
+                "caption": ""
+            },
+            {
+                "src": "picture40.jpg",
+                "caption": ""
+            },
+            {
+                "src": "picture41.jpg",
+                "caption": ""
+            },
+            {
+                "src": "picture42.jpg",
+                "caption": ""
+            }
+        ],
+        "city": "新加坡",
+        "title": "新加坡",
+        "order": 2
+    },
+    {
+        "marker_img": "picture62.jpg",
+        "position": [
+            151.16691,
+            -33.93635
+        ],
+        "content": "4567890",
+        "imgs": [
+            {
+                "src": "picture62.jpg",
+                "caption": ""
+            },
+            {
+                "src": "picture63.jpg",
+                "caption": ""
+            },
+            {
+                "src": "picture64.jpg",
+                "caption": ""
+            },
+            {
+                "src": "picture65.jpg",
+                "caption": ""
+            },
+            {
+                "src": "picture66.jpg",
+                "caption": ""
+            },
+            {
+                "src": "picture67.jpg",
+                "caption": ""
+            }
+        ],
+        "city": "澳大利亚",
+        "title": "澳大利亚",
+        "order": 3
+    },
+    {
+        "marker_img": "picture28.jpg",
+        "position": [
+            168.66529,
+            -45.03259
+        ],
+        "content": "5678901",
+        "imgs": [
+            {
+                "src": "picture28.jpg",
+                "caption": ""
+            },
+            {
+                "src": "picture29.jpg",
+                "caption": ""
+            },
+            {
+                "src": "picture30.jpg",
+                "caption": ""
+            },
+            {
+                "src": "picture31.jpg",
+                "caption": ""
+            },
+            {
+                "src": "picture32.jpg",
+                "caption": ""
+            },
+            {
+                "src": "picture33.jpg",
+                "caption": ""
+            },
+            {
+                "src": "picture34.jpg",
+                "caption": ""
+            },
+            {
+                "src": "picture35.jpg",
+                "caption": ""
+            }
+        ],
+        "city": "新西兰",
+        "title": "新西兰",
+        "order": 4
+    }
+]
+
+function get_data() {
+    return map_data;
+}
+
